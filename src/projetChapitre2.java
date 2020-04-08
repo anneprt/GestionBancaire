6e28ef5b63e15a0c91863e80f79fc0370b374e82
Anne <patesdegeek@gmail.com>
2020-04-08 22:52:20 +0200

    creation affichage menu

--- src/projetChapitre2.java
+++ src/projetChapitre2.java
@@ -12,6 +12,15 @@ public class projetChapitre2 {
 
         //Affichage du menu principal
         System.out.println("1.Créer un compte");
+        System.out.println("2.Afficher un compte");
+        System.out.println("3.Créer une ligne comptable");
+        System.out.println("4.Sortir");
+        System.out.println("5.Aide");
+        System.out.println();
+        System.out.print("Votre choix : ");
+
+        choix = lectureClavier.nextByte();
+
 
     }
 }
