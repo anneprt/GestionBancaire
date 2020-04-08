1de975d80bd860e0d347fa207a7cd39d326da15b
Anne <patesdegeek@gmail.com>
2020-04-08 22:47:18 +0200

    declaration variables

---
+++ src/projetChapitre1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class projetChapitre2 {
+    public static void main(String[] args) {
+        byte choix;
+        char typeCompte = '\0';
+        double valeur_courante = 0.0;
+        double taux = 0.0;
+        long numeroCompte = 0;
+        long numeroLu = 0;
+        Scanner lectureClavier = new Scanner(System.in);
+
+        //Affichage du menu principal
+        System.out.println("1.Créer un compte");
+
+    }
+}
