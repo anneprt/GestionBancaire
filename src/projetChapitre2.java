5a5e1e0269699c8fcebe9d6b7000fcfbe67d4cf4
Anne <patesdegeek@gmail.com>
2020-04-14 22:27:28 +0200

    choix option 2.3.4.5

--- src/projetChapitre2.java
+++ src/projetChapitre2.java
@@ -13,6 +13,7 @@ public class projetChapitre2 {
         choix = lectureClavier.nextByte();
         //Selon option choisie
         switch (choix) {
+
             case 1:
                 System.out.print("Type de compte [Types possibles : (c) courant, (j) joint ,(e) épargne] :");
                 typeCompte = lectureClavier.next().charAt(0);
@@ -21,10 +22,51 @@ public class projetChapitre2 {
                 System.out.print("Première valeur créditée : ");
                 valeur_courante = lectureClavier.nextDouble();
                 //option 1 : cas du compte epargne, demande du taux
-                if (typeCompte == 'E') {
+                if (typeCompte == 'e') {
                     System.out.print("Taux de placement : ");
                     taux = lectureClavier.nextDouble();
                 }
+                break;
+            case 2:
+                //demander à l'utilisateur un numero de compte à afficher
+                System.out.print("Quel compte souhaitez-vous afficher ? : ");
+                numeroLu = lectureClavier.nextLong();
+                //verifier que le compte existe
+                if (numeroLu == numeroCompte) {
+                    System.out.print("Le compte" + numeroCompte + "est un compte");
+                    if (typeCompte == 'c') {
+                        System.out.println("courant");
+                    } else if (typeCompte == 'j') {
+                        System.out.println("joint");
+                    } else if (typeCompte == 'e') {
+                        System.out.println("épargne dont le taux de placement est de " + taux);
+                    }
+                    System.out.println("Première valeur créditée" + valeur_courante);
+                } else {
+                    //compte non reconnu
+                    System.out.print("Le système ne reconnaît pas le compte" + numeroLu);
+                }
+                break;
+            case 3:
+                System.out.print("Cette fonction n'est pas encore implémentée");
+                break;
+            case 4:
+                System.out.println("Merci et à bientôt");
+                System.exit(0);
+                break;
+            case 5:
+                //option 5 menu d'aide
+                System.out.println("Option 1. Pour créer un compte Courant entrer c ");
+                System.out.println("Pour créer un compte Joint entrer c ");
+                System.out.println("Pour créer un compte Epargne entrer e");
+                System.out.print("Puis, entrer le numero du compte, et");
+                System.out.println(" sa premiere valeur créditee ");
+                System.out.println("Dans le cas d'un compte epargne, entrer le taux ");
+                System.out.println("Option 2. Le système affiche les données du compte choisi ");
+                System.out.println("Option3. Ecrire une ligne comptable");
+                System.out.println("Option 4. Pour quitter le programme");
+                System.out.println("Option 5. Pour afficher de l'aide");
+                break;
 
         }
 
@@ -39,40 +81,7 @@ public class projetChapitre2 {
         System.out.print("Votre choix : ");
 
 
-        //option 2 :affichage d'un compte
 
-        //demander à l'utilisateur un numero de compte à afficher
-        System.out.print("Quel compte souhaitez-vous afficher ? : ");
-        numeroLu = lectureClavier.nextLong();
-
-        //si le numero de compte existe
-        System.out.print("Le compte" + numeroCompte + "est un compte");
-
-        //cas du compte épargne
-        System.out.println("épargne dont le taux de placement est de " + taux);
-        System.out.println("Première valeur créditée" + valeur_courante);
-
-        //le compte n'existe pas affichage d'un message d'erreur
-        System.out.print("Le système ne reconnaît pas le compte" + numeroLu);
-
-        //option 3 option non programme, affichage message
-        System.out.print("Cette fonction n'est pas encore implémentée");
-
-        //option 4 sortir du programme
-        System.out.println("Merci et à bientôt");
-        System.exit(0);
-
-        //option 5 menu d'aide
-        System.out.println("Option 1. Pour créer un compte Courant entrer c ");
-        System.out.println("Pour créer un compte Joint entrer c ");
-        System.out.println("Pour créer un compte Epargne entrer e");
-        System.out.print("Puis, entrer le numero du compte, et");
-        System.out.println(" sa premiere valeur créditee ");
-        System.out.println("Dans le cas d'un compte epargne, entrer le taux ");
-        System.out.println("Option 2. Le système affiche les données du compte choisi ");
-        System.out.println("Option3. Ecrire une ligne comptable");
-        System.out.println("Option 4. Pour quitter le programme");
-        System.out.println("Option 5. Pour afficher de l'aide");
 
 
     }
