6e3950efdbfd0ba1db28655a4fdfa26cfcdc2609
Anne <patesdegeek@gmail.com>
2020-04-20 18:47:49 +0200

    definition+appel fonctions menuPrincipal,sortir,aide

--- src/projetChapitre2.java
+++ src/projetChapitre2.java
@@ -12,16 +12,7 @@ public class projetChapitre2 {
 
         //Affichage du menu principal
         do {
-            System.out.println("1.Créer un compte");
-            System.out.println("2.Afficher un compte");
-            System.out.println("3.Créer une ligne comptable");
-            System.out.println("4.Sortir");
-            System.out.println("5.Aide");
-            System.out.println();
-            System.out.print("Votre choix : ");
-            choix = lectureClavier.nextByte();
-
-
+            choix = menuPrincipal();
             //Selon option choisie
             switch (choix) {
 
@@ -64,26 +55,50 @@ public class projetChapitre2 {
                     System.out.println("Cette fonction n'est pas encore implémentée");
                     break;
                 case 4:
-                    System.out.println("Merci et à bientôt");
-                    System.exit(0);
+                    sortir();
                     break;
                 case 5:
                     //option 5 menu d'aide
-                    System.out.println("Option 1. Pour créer un compte Courant entrer c ");
-                    System.out.println("Pour créer un compte Joint entrer c ");
-                    System.out.println("Pour créer un compte Epargne entrer e");
-                    System.out.print("Puis, entrer le numero du compte, et");
-                    System.out.println(" sa premiere valeur créditee ");
-                    System.out.println("Dans le cas d'un compte epargne, entrer le taux ");
-                    System.out.println("Option 2. Le système affiche les données du compte choisi ");
-                    System.out.println("Option 3. Ecrire une ligne comptable");
-                    System.out.println("Option 4. Pour quitter le programme");
-                    System.out.println("Option 5. Pour afficher de l'aide");
+                    aide();
                     break;
                 default:
                     System.out.println("Cette option n'existe pas ");
             }
 
         } while (choix != 4);
+    }
+
+    public static byte menuPrincipal() {
+        byte tmp;
+        Scanner lectureClavier = new Scanner(System.in);
+        System.out.println("1.Créer un compte");
+        System.out.println("2.Afficher un compte");
+        System.out.println("3.Créer une ligne comptable");
+        System.out.println("4.Sortir");
+        System.out.println("5.Aide");
+        System.out.println();
+        System.out.print("Votre choix : ");
+        tmp = lectureClavier.nextByte();
+        return tmp;
+
+    }
+
+    public static void sortir() {
+        System.out.println("Merci et à bientôt");
+        System.exit(0);
+    }
+
+    public static void aide() {
+        System.out.println("Option 1. Pour créer un compte Courant entrer c ");
+        System.out.println("Pour créer un compte Joint entrer c ");
+        System.out.println("Pour créer un compte Epargne entrer e");
+        System.out.print("Puis, entrer le numero du compte, et");
+        System.out.println(" sa premiere valeur créditee ");
+        System.out.println("Dans le cas d'un compte epargne, entrer le taux ");
+        System.out.println("Option 2. Le système affiche les données du compte choisi ");
+        System.out.println("Option 3. Ecrire une ligne comptable");
+        System.out.println("Option 4. Pour quitter le programme");
+        System.out.println("Option 5. Pour afficher de l'aide");
+
     }
 }
